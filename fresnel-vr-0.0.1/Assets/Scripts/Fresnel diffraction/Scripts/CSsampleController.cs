34403302dc4ef49da2538897b3b9c9515eefc9c4
Pucong Li <140274533+MWhale233@users.noreply.github.com>
2025-04-14 21:21:16 +0200

    Red color is successfully rendered using compute shader

---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/Scripts/CSsampleController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:462c64d891773bf32cbc0d6d8d312d830692a45658e5d61f02a6af5c488d6b4f
+size 1153
---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/Shaders/CSsample.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b138b6e43700b28cab1f3471c851b667483f095e44caa4b1727ca6569639b34e
+size 1065
