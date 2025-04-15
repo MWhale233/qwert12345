af9fada7f6eb6e998fe90553fca8c590ad86ae5c
Pucong Li <140274533+MWhale233@users.noreply.github.com>
2025-04-15 22:57:14 +0200

    3D texture CS is rendered
    test using Result[id.xyz]

---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/15.05.2025/FresnelCS.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4b48f8619f003e79f262d6f48fc20c550fcf2215464f85ff65c2a7bb26219a3
+size 1112
---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/15.05.2025/FresnelCS.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63c4410208df0ab8a6f438a85891d83806798195e66b0e5be7647ee3eb3fbef4
+size 1232
