6f2afe7f17eedbef438c66a5f560078d83956dfe
Pucong Li <140274533+MWhale233@users.noreply.github.com>
2025-04-25 21:29:08 +0200

    creating RGB 3D CS cube

---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/25.04.2025/RGBCS.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb91ce14efce5a4a05b87db8fc4414e93d0851920a4c06a64ee36e15634c21c8
+size 1712
---
+++ fresnel-vr-0.0.1/Assets/Scripts/Fresnel diffraction/25.04.2025/RGBCS.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6455b032e371b9450801647b42de6e27ee2037f4934692556eb197b86d2dddd
+size 1299
